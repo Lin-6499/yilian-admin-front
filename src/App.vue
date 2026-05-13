3d34907b9dd6bbf7f46b7dc0573ac9c3f9b8ab8e
Lin-6499 <lin_6499@qq.com>
2026-05-13 19:37:33 +0800

    更改登陆界面

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,24 @@
 <template>
-  <router-view />
+  <ClickSpark>
+    <router-view />
+  </ClickSpark>
 </template>
+<script>
+import ClickSpark from './components/ClickSpark/ClickSpark.vue'
+import Prism from "./components/Prism/Prism.vue";
+import {defineComponent} from "vue";
 
+export default defineComponent({
+  components: {Prism, ClickSpark}
+})
+
+</script>
 <style>
 /* Global styles can also go here or in style.css */
+html, body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  overflow: hidden;   /* 禁止滚动条 */
+}
 </style>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -11,27 +11,42 @@
           <el-input v-model="loginForm.username" prefix-icon="User" placeholder="用户名"></el-input>
         </el-form-item>
         <el-form-item prop="password">
-          <el-input v-model="loginForm.password" prefix-icon="Lock" type="password" placeholder="密码" show-password></el-input>
+          <el-input style="z-index: 2" v-model="loginForm.password" @focus="isActive=true" @blur="isActive=false" prefix-icon="Lock" type="password" placeholder="密码" show-password></el-input>
         </el-form-item>
         <el-form-item>
           <el-button type="primary" :loading="loading" @click="handleLogin" style="width: 100%">登录</el-button>
         </el-form-item>
       </el-form>
+      <img class="img" :class="isActive?'img-active':''" src="../../assets/chicken.gif"></img>
     </el-card>
+    <ParticlesBg
+        :key="isDark.toString()"
+        class="particles-bg"
+        :quantity="100"
+        :ease="100"
+        :color="isDark ? '#ffffff' : '#ffffff'"
+        :staticity="10"
+        refresh
+    />
   </div>
+
 </template>
 
 <script setup lang="ts">
-import { reactive, ref } from 'vue'
+import { reactive, ref, computed} from 'vue'
 import { useRouter } from 'vue-router'
 import { ElMessage } from 'element-plus'
 import axios from 'axios'
+import { useColorMode } from "@vueuse/core";
 import type { FormInstance, FormRules } from 'element-plus'
+import ParticlesBg from "../../components/ParticlesBg/ParticlesBg.vue";
 
+const isDark = computed(() => useColorMode().value === "dark");
 const router = useRouter()
 const loginFormRef = ref<FormInstance>()
 const loading = ref(false)
 
+const isActive = ref(false)
 const loginForm = reactive({
   username: '',
   password: ''
@@ -72,14 +87,46 @@ const handleLogin = async () => {
   justify-content: center;
   align-items: center;
   height: 100vh;
-  background-color: #f0f2f5;
+  background-color: #2b2a2a;
 }
 
 .login-card {
   width: 400px;
+  background: linear-gradient(to bottom, #ffffff, #2b2a2a);
+  opacity: 0.8;
+  backdrop-filter: blur(5px);
+  -webkit-backdrop-filter: blur(5px);
 }
 
 .login-header {
   text-align: center;
 }
+.particles-bg {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+  pointer-events: none;
+}
+
+:deep(.el-card) {
+ border: none;
+}
+
+.img{
+  width:24px ;
+  height:24px ;
+  position: absolute;
+  top:160px;
+  left: 180px;
+  z-index: 1;
+  transition: top 0.3s
+}
+.img-active{
+  top: 175px;
+}
+
 </style>
